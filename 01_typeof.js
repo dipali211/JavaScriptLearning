69a90b146abfdc1170867f931426ba15529d4f5a
dipali211 <jangaledipali7@gmail.com>
2022-12-09 14:40:03 +0530

    swaping var, typeof.js
    swaping var, typeof.js

---
+++ 01_typeof.js
@@ -0,0 +1,26 @@
+var bankName="SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+console.log("***************");
+
+var marks="90%";
+console.log(marks);
+console.log(typeof marks);
+console.log("***************");
+
+var isworking=true;
+console.log(isworking);
+console.log(typeof isworking);
+console.log("***************");
+
+var totalcount="Hundred and seven";
+console.log(totalcount);
+console.log(typeof totalcount);
+console.log("****************");
+
+var totalcount=107;
+console.log(totalcount);
+console.log(typeof totalcount);
+
+
+
---
+++ 03_swap_var.js
@@ -0,0 +1,14 @@
+console.log("before swaping");
+
+var sweety="Sweety";
+var cutie="cutie"
+
+console.log("sweety=" +sweety , "cutie=" +cutie);
+
+console.log("After Swaping");
+
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+
+console.log("sweety=" +sweety , "cutie=" +cutie);
