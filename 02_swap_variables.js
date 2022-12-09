b0c980bc7796b9ed210d99ffd99cd9c133b461fa
dipali211 <jangaledipali7@gmail.com>
2022-12-09 10:55:20 +0530

    swaping variables.js
    swaping variables.js

---
+++ 02_swap_variables.js
@@ -0,0 +1,13 @@
+console.log("Before Swaping");
+var num1= 1000;
+var num2= 2000;
+var num3= 3000;
+
+console.log("num1="+num1, "num2="+num2, "num3="+num3 );
+console.log("After Swaping");
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+
+console.log("num1="+num1, "num2="+num2, "num3="+num3);
