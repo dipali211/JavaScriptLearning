e32d4d8b9f53be78172a497126e8ca5c3668e6ff
dipali211 <jangaledipali7@gmail.com>
2022-12-10 11:06:48 +0530

    function call assign
    function call assign

---
+++ 02_function.js
@@ -0,0 +1,23 @@
+function show() {
+    //task to perform
+    console.log("college name");
+    console.log("IMR");
+    
+}
+show();//function call or function invocation
+
+function showDetails(){
+
+}
+showDetails("Dipa jangale")
+function swapValues(value1,value2){
+    console.log("before swaping",value1,value2);
+    var temp=value1;
+    value1=value2;
+    value2=temp;
+    console.log("after swaping",value1,value2);
+}
+swapValues(100,200);
+swapValues(2,4);
+swapValues("dips","jangale");
+swapValues("you","me");
