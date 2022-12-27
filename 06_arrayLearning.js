adb4fdb43d8317b354c563b44cbd801407750ff6
dipali211 <jangaledipali7@gmail.com>
2022-12-27 09:11:40 +0530

    array.js
    array.js

--- 06_arrayLearning.js
+++ 06_arrayLearning.js
@@ -9,7 +9,7 @@ for (let index = 0; index < arrayOfSalaries.length; index++) {
 }
 console.log(`Total Salary of all employee is : ${sumOfSal}`);
 console.log(`======== push() methods=========`);
-let arrayOfNum = [4, 5, 6, 8, 9, 1];
+var arrayOfNum = [4, 5, 6, 8, 9, 1];
 console.log(arrayOfNum);
 arrayOfNum.push(3);
 console.log(arrayOfNum);
@@ -22,73 +22,73 @@ arrayOfNum.unshift(99, 111, 333);
 console.log(arrayOfNum);
 
 
-// console.log(`======== pop() methods=========`);
-// var arrayOfNum = [4, 5, 6, 8, 9, 33];
-// let popResult = arrayOfNum.pop();
-// console.log(popResult);
-// console.log(arrayOfNum);
+console.log(`======== pop() methods=========`);
+var arrayOfNum = [4, 5, 6, 8, 9, 33];
+let popResult = arrayOfNum.pop();
+console.log(popResult);
+console.log(arrayOfNum);
 
 
-// console.log(`======== shift() methods=========`);
-// var arrayOfNum = [4, 5, 6, 8, 9, 33];
-// let shiftResult = arrayOfNum.shift();
-// console.log(shiftResult);
-// console.log(arrayOfNum);
+console.log(`======== shift() methods=========`);
+var arrayOfNum = [4, 5, 6, 8, 9, 33];
+let shiftResult = arrayOfNum.shift();
+console.log(shiftResult);
+console.log(arrayOfNum);
 
 
-// console.log(`======== slice() methods=========`);
-// var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
-// let sliceResult = arrayOfNum.slice(3);
-// let sliceRes = arrayOfNum.slice(2, 5);
-// console.log(arrayOfNum);
-// console.log(sliceResult);
-// console.log(sliceRes);
+console.log(`======== slice() methods=========`);
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+let sliceResult = arrayOfNum.slice(3);
+let sliceRes = arrayOfNum.slice(2, 5);
+console.log(arrayOfNum);
+console.log(sliceResult);
+console.log(sliceRes);
 
 
-// console.log(`======== splice() methods=========`);
-// var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
-// console.log(arrayOfNum);
-// let spliceResult = arrayOfNum.splice(3);
-// console.log(`After using splice(3) method `);
-// console.log("Array after deleting elements ", arrayOfNum);
-// console.log("Total deleted elements: ", spliceResult);
+console.log(`======== splice() methods=========`);
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+console.log(arrayOfNum);
+let spliceResult = arrayOfNum.splice(3);
+console.log(`After using splice(3) method `);
+console.log("Array after deleting elements ", arrayOfNum);
+console.log("Total deleted elements: ", spliceResult);
 
-// var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
-// console.log(arrayOfNum);
-// let spliceRes = arrayOfNum.splice(2, 3);
-// console.log(`After using splice(2, 3) method `);
-// console.log("Array after deleting elements ", arrayOfNum);
-// console.log("Total deleted elements: ", spliceRes);
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+console.log(arrayOfNum);
+let spliceRes = arrayOfNum.splice(2, 3);
+console.log(`After using splice(2, 3) method `);
+console.log("Array after deleting elements ", arrayOfNum);
+console.log("Total deleted elements: ", spliceRes);
 
 
-// console.log("====== Inserting element in the array =======");
-// var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
-// console.log(arrayOfNum);
-// arrayOfNum.splice(2, 0, 88);
-// console.log(arrayOfNum);
-// arrayOfNum.splice(1, 0, 55, 99, 22);
-// console.log(arrayOfNum);
+console.log("====== Inserting element in the array =======");
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+console.log(arrayOfNum);
+arrayOfNum.splice(2, 0, 88);
+console.log(arrayOfNum);
+arrayOfNum.splice(1, 0, 55, 99, 22);
+console.log(arrayOfNum);
 
-// console.log("====== Replacing element in the array =======");
-// var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
-// console.log(arrayOfNum);
-// arrayOfNum.splice(3, 1, 22);
-// console.log(arrayOfNum);
-// console.log("====== Replacing element in the array when splice(2, 3, 99, 77 ) =======");
-// var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
-// arrayOfNum.splice(2, 3, 99, 77 );
-// console.log(arrayOfNum);
+console.log("====== Replacing element in the array =======");
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+console.log(arrayOfNum);
+arrayOfNum.splice(3, 1, 22);
+console.log(arrayOfNum);
+console.log("====== Replacing element in the array when splice(2, 3, 99, 77 ) =======");
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+arrayOfNum.splice(2, 3, 99, 77 );
+console.log(arrayOfNum);
 
-// console.log(`======== for of loop ==========`);
-// var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
-// console.log(arrayOfNum);
-// for (const element of arrayOfNum) {
-//     console.log(element);
-// }
-// console.log("==========include() ========");
-// var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
-// let isAvailable = arrayOfNum.includes(9);
-// console.log(isAvailable);
+console.log(`======== for of loop ==========`);
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+console.log(arrayOfNum);
+for (const element of arrayOfNum) {
+    console.log(element);
+}
+console.log("==========include() ========");
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+let isAvailable = arrayOfNum.includes(9);
+console.log(isAvailable);
 
 console.log(`======== join() ==========`);
 var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
