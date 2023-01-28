5808f37e1b51c7efebf0db73471ba602dc5f553a
dipali211 <jangaledipali7@gmail.com>
2023-01-28 15:51:28 +0530

    arrowfunction.js
    arrowfunction.js

---
+++ 07_function.js
@@ -0,0 +1,6 @@
+var square =function(num){
+    console.log("square of", num,"=",num*num);
+}
+square(5);
+square(6);
+console.log("type of square=", typeof square);
---
+++ 12_arrowFunction.js
@@ -0,0 +1,82 @@
+// let sentence = ()=>{
+//     console.log(`1) Simple message with arrow function`);
+//     console.log(`Good Morning, Today is Monday`);
+// }
+// sentence();
+// console.log(``);
+
+// console.log(`2) Multiplication of three numbers and Multiplication of three numbers with one default value`);
+// let multiply = (n1, n2, n3)=>{
+//     let mul = n1 * n2 * n3;
+//     console.log(`a) Multiplication of 5, 5, 2 is: ${mul}`);
+// }
+// multiply(5, 5, 2);
+
+// let multiply1 = (n1, n2, n3=1)=>{
+//     let mul = n1 * n2 * n3;
+//     console.log(`b) Multiplication of 10, 4 with default value 1 is: ${mul}`);
+// }
+// multiply1(10, 4);
+// console.log(``);
+
+// console.log(`3) The addition of 5 parameters and return it's value`);
+// let addition = (n1, n2, n3, n4, n5)=> {
+//     let add = n1 + n2 + n3 + n4 +n5;
+// // console.log(add);
+//  console.log(`The addition is: ${add}`);
+// }
+// addition(100, 100, 200, 349, 756);
+
+// let message = (value1, value2, value3, value4, value5)=>{
+// let addRes = value1 + value2 + value3 + value4 + value5;
+// console.log(`The addition is: ${addRes}`);
+// }
+// message("I am ", "learning ", "ES6 ", "features ", "in depth.")
+// let value = (n1, n2, n3, n4, n5) => {
+//     console.log(n1+n2+n3+n4+n5);
+// }
+// let addTotal = value(100, 100, 200, 349, 756);
+// //console.log(`Addition of given number is :- ${addTotal}`);
+
+// let string = (n1, n2, n3, n4, n5) => {
+//     console.log(n1+n2+n3+n4+n5);
+   
+// }
+// string("I am", "learning", "ES6", "features", "in depth");
+
+
+
+
+let display = ()=>{
+    console.log(`Good Morning, Today is Monday`);
+}
+display();
+console.log(`---------------------------------------------------`);
+
+
+let mul = (n1, n2, n3) => {
+    console.log(n1*n2*n3);
+}
+let multotal = mul(5, 5, 2);
+console.log("Multiplication of given number is :- ",multotal);
+
+let mult = (n1, n2, n3=1) => {
+    console.log(n1*n2*n3);
+}
+let multTotal = mult(10, 4);
+console.log(multTotal);
+//console.log(`Multiplication of given number is :- ${add}`);
+console.log(`----------------------------------------------------`);
+
+
+let value = (n1, n2, n3, n4, n5) => {
+    console.log(n1+n2+n3+n4+n5);
+}
+let addTotal = value(100, 100, 200, 349, 756);
+//console.log(`Addition of given number is :- ${addTotal}`);
+
+let string = (n1, n2, n3, n4, n5) => {
+    console.log(n1+n2+n3+n4+n5);
+   
+}
+string("I am", "learning", "ES6", "features", "in depth");
